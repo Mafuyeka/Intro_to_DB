f9fb5decd40a87b80eee579039557e4431976df7
Mafuyeka <tshepisoroshina@gmail.com>
2025-07-04 01:21:59 -0700

    Updated

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,12 @@
-SELECT *
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT, 
+    COLUMN_KEY, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
